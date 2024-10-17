fbb3fae8de151ed4b725ba330837e2840438549c
FabricioMeneze5 <fabricio-menezes@live.com>
2024-10-17 22:25:48 +0100

    ADD: test 'Deve renderizar os comentarios'

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,33 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve renderizar os comentarios', () => {
+        const clickButtom = () => {
+            fireEvent.click(screen.getByTestId('btn-comantar'))
+        }
+        render(<PostComment/>);
+        fireEvent.change(screen.getByTestId('text-area'), {
+            target: {
+                value: 'comentario 1'
+            }
+        })
+        clickButtom()
+
+        fireEvent.change(screen.getByTestId('text-area'), {
+            target: {
+                value: 'comentario 2'
+            }
+        })
+        clickButtom()
+        
+        expect(screen.getAllByTestId('post-comment-id')).toHaveLength(2);
+    });
+
+    
 });
